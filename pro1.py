7851a0cd945b7b7c263c401e395450aeaf314835
SowmigaK <sowmigakarthikeyan@gmail.com>
2026-09-07 17:27:53 +0530

    learnt - data type,variable,opeartor,fstring

---
+++ pro1.py
@@ -0,0 +1,16 @@
+print("'hello'")
+print('"hello"')
+print("\"my name is arun\"")
+print(8//2)
+print(8/2)
+print(8%2)
+print(7//2,7/2,7%2)
+print("Name: Arun\nAge: 21\nDegree: B.sc")
+name = "Arun"
+age =21
+roll_no ='101'
+print(name)
+name2 = input("Enter your name: ")
+print(f"The name of second student is: {name2} And the Age of first student: {age}")
+
+
