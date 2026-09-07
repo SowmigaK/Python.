99f1116ce093808f0526d049d3b46133173c514b
SowmigaK <sowmigakarthikeyan@gmail.com>
2026-09-07 22:38:07 +0530

    task_02_variables.py

---
+++ pro2.py
@@ -0,0 +1,48 @@
+student1_name = "Anu"
+student1_dob = "15-05-2005"
+student1_roll_no = "101"
+student1_degree = "B.Sc"
+student1_gender = "Female"
+student1_course_name = "Mathematics"
+student1_course_code = "MAT101"
+student1_credits = 4
+
+student2_name = "Priya"
+student2_dob = "20-08-2005"
+student2_roll_no = "102"
+student2_degree = "B.Sc"
+student2_gender = "Female"
+student2_course_name = "Mathematics"
+student2_course_code = "MAT101"
+student2_credits = 4
+
+student3_name = "Kaviya"
+student3_dob = "10-02-2006"
+student3_roll_no = "103"
+student3_degree = "B.Sc"
+student3_gender = "Female"
+student3_course_name = "Mathematics"
+student3_course_code = "MAT101"
+student3_credits = 4
+
+student4_name = "Divya"
+student4_dob = "25-11-2005"
+student4_roll_no = "104"
+student4_degree = "B.Sc"
+student4_gender = "Female"
+student4_course_name = "Mathematics"
+student4_course_code = "MAT101"
+student4_credits = 4
+
+student5_name = "Meena"
+student5_dob = "05-03-2006"
+student5_roll_no = "105"
+student5_degree = "B.Sc"
+student5_gender = "Female"
+student5_course_name = "Mathematics"
+student5_course_code = "MAT101"
+student5_credits = 4
+
+print(student4_name)
+print(student5_course_name)
+print(student2_roll_no)
